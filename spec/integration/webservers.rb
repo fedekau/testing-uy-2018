7011952ab05912a960bb2ccd087188a002bab8b0
fedekau <fede.kau@gmail.com>
2018-05-17 19:45:22 -0300

    Comment playbook tasks and tests

--- spec/integration/webservers.rb
+++ spec/integration/webservers.rb
@@ -2,11 +2,11 @@
 
 title 'nginx'
 
-describe package('nginx') do
-  it { should be_installed }
-end
+# describe package('nginx') do
+#   it { should be_installed }
+# end
 
-describe sysv_service('nginx') do
-  it { should be_enabled }
-  it { should be_running }
-end
+# describe sysv_service('nginx') do
+#   it { should be_enabled }
+#   it { should be_running }
+# end
